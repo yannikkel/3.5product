63bd1c0739ece4e0493540bf44e556597d367b47
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-06-02 16:01:55 +0300

    Create Book.java

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Book extends Product {
+    private String author;
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+}
