9fa1402c653565cdfd98fa70491648757216c378
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-06-02 16:07:11 +0300

    int

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Smartphone extends Product {
+    private String manufacturer;
+
+    public Smartphone(int id, String name, int price, String manufacturer) {
+        super(id, name, price);
+        this.manufacturer = manufacturer;
+    }
+}
